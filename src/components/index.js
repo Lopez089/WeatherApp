9233c8f882f7f8b7339937dc5d586bd65aca39cb
LopezDev <aragonlopezjuan87@gmail.com>
2023-06-16 16:41:10 +0200

    create storybook and component logo

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export * from './logo/logo';
 export * from './menu/menu';
-export * from './menu/menu.test';
 export * from './nav/nav';
 export * from './search/search';
 export * from './weather/weather';
---
+++ src/stories/Button.stories.js
@@ -0,0 +1,18 @@
+import { Logo } from '../components';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+export default {
+  title: 'Components/Logo',
+  component: Logo,
+  tags: ['autodocs'],
+  argTypes: {
+
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Logo_default = {
+  args: {
+
+  },
+};
